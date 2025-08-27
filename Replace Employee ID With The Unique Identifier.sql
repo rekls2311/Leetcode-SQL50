20de90cfd5556339ed07835b51b3ae9dfda6b55c
rekls2311 <80893626+rekls2311@users.noreply.github.com>
2025-08-28 09:25:10 +1000

    Update Replace Employee ID With The Unique Identifier.sql

--- Replace Employee ID With The Unique Identifier.sql
+++ Replace Employee ID With The Unique Identifier.sql
@@ -1,6 +1,5 @@
 --Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 
-Return the result table in any order.
 SELECT EmployeeUNI.unique_id, Employees.name
 FROM Employees
 LEFT JOIN EmployeeUNI
