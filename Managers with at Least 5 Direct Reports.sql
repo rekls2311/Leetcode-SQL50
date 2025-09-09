1dc4ab0097574a7c11fd94725030080c746f7a4e
Hoang Long Nguyen <80893626+rekls2311@users.noreply.github.com>
2025-09-09 10:46:53 +1000

    Create Managers with at Least 5 Direct Reports.sql

---
+++ Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,16 @@
+--Option 1: subquery
+select name
+from employee
+where id = (select managerId
+from employee
+group by managerId
+having count(managerId) >=5)
+
+--option 2: Join
+
+# Write your MySQL query statement below
+select e1.name
+from employee AS e1
+JOIN employee as e2 ON e1.id = e2.managerId
+group by e1.id
+HAVING COUNT(e2.managerId) >=5
